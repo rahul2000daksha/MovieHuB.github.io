e6878a57d8cd51233398288e2a4546ee5d846bf1
Rahul Daksha <rahul2000daksha@gmail.com>
2022-10-11 13:41:24 +0530

    Movie hub
    Movie Hub

---
+++ C/src/components/featured/Featured.jsx
@@ -0,0 +1,73 @@
+import { InfoOutlined, PlayArrow } from "@material-ui/icons";
+import "./featured.scss";
+import axios from "axios";
+import { useEffect, useState } from "react";
+
+export default function Featured({ type, setGenre }) {
+
+  const [content, setContent] = useState({});
+
+  useEffect(() => {
+    const getRandomContent = async () => {
+      try {
+        const res = await axios.get(`/movies/random?type=${type}`, {
+          headers: {
+            token:
+              "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYyNmJjMjVlMWZiMGZmNzRhNWQ1ODJhYyIsImlzQWRtaW4iOnRydWUsImlhdCI6MTY2MTk1NzEzOSwiZXhwIjoxNjYyMzg5MTM5fQ.-mrE0AqlAHrXIjIToQ_qsE6ek8OcQmHZ1YKlzUGVm40"
+          },
+        });
+        setContent(res.data[0]);
+      } catch (err) {
+        console.log(err);
+      }
+    };
+    getRandomContent();
+  }, [type]);
+
+  console.log(content);
+
+  return (
+    <div className="featured">
+      {type && (
+        <div className="category">
+          <span>{type === "movies" ? "Movies" : "Series"}</span>
+          <select
+            name="genre"
+            id="genre"
+            onChange={(e) => setGenre(e.target.value)}
+          >
+            <option>Genre</option>
+            <option value="adventure">Adventure</option>
+            <option value="comedy">Comedy</option>
+            <option value="crime">Crime</option>
+            <option value="fantasy">Fantasy</option>
+            <option value="historical">Historical</option>
+            <option value="horror">Horror</option>
+            <option value="romance">Romance</option>
+            <option value="sci-fi">Sci-fi</option>
+            <option value="thriller">Thriller</option>
+            <option value="western">Western</option>
+            <option value="animation">Animation</option>
+            <option value="drama">Drama</option>
+            <option value="documentary">Documentary</option>
+          </select>
+        </div>
+      )}
+      <img src={content.img} alt="" />
+      <div className="info">
+        <img src={content.imgTitle} alt="" />
+        <span className="desc">{content.desc}</span>
+        <div className="buttons">
+          <button className="play">
+            <PlayArrow />
+            <span>Play</span>
+          </button>
+          <button className="more">
+            <InfoOutlined />
+            <span>Info</span>
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+}
---
+++ C/src/pages/home/Home.jsx
@@ -0,0 +1,45 @@
+import Navbar from "../../components/navbar/Navbar";
+import Featured from "../../components/featured/Featured";
+import "./home.scss";
+import List from "../../components/list/List";
+import { useEffect, useState } from "react";
+import axios from "axios";
+
+const Home = ({type}) => {
+  const [lists, setLists] = useState([]);
+    const [genre, setGenre] = useState(null);
+
+  useEffect(() => {
+    const getRandomLists = async () => {
+      try {
+        const res = await axios.get(
+          `lists${type ? "?type=" + type : ""}${genre ? "&genre=" + genre : ""
+          }`,
+          {
+            headers: {
+              token:
+              "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYyNmJjMjVlMWZiMGZmNzRhNWQ1ODJhYyIsImlzQWRtaW4iOnRydWUsImlhdCI6MTY2MTk1NzEzOSwiZXhwIjoxNjYyMzg5MTM5fQ.-mrE0AqlAHrXIjIToQ_qsE6ek8OcQmHZ1YKlzUGVm40"
+          },
+          }
+        );
+        setLists(res.data);
+      } catch (err) {
+        console.log(err);
+      }
+    };
+    getRandomLists();
+  }, [type, genre]);
+  console.log(lists)
+
+  return (
+    <div className="home">
+      <Navbar />
+      <Featured type={type} setGenre={setGenre} />
+      {lists.map((list) => (
+        <List list={list} />
+      ))}
+    </div>
+  );
+};
+
+export default Home;
